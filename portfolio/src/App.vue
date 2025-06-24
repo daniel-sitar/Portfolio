a08ef7f8c82f8a8b49da2f9c9916674106eee9c5
daniel.sitar <sitarpublic@gmail.com>
2025-06-24 12:15:47 +0200

    del helloworld component

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,10 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">Home</router-link>
   <router-view></router-view>
 </template>
 
