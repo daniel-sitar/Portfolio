9877b28fd4e8ed0aaa4ea69867524fe98294d35c
daniel.sitar <sitarpublic@gmail.com>
2025-06-27 17:57:07 +0200

    add aligngment + text

--- portfolio/src/Views/HomeView.vue
+++ portfolio/src/Views/HomeView.vue
@@ -1,21 +1,36 @@
 <template>
     <div class="home">
-        <div class="bg-slate-500 h-screen w-screen grid grid-cols-10 grid-rows-10 gap-4">
+        <div class="bg-obsidian-600 h-screen w-screen grid grid-cols-10 grid-rows-10 gap-4">
             <div class="col-span-1 row-span-10 bg-dark-400">
-                <div class="h-full bg-purple-900 grid grid-cols-3 grid-rows-5">
-                    <div class="col-span-3 row-span-1 bg-red-500 items-center">
-                        <div class="flex items-center justify-center justify-items-center h-full">
-                            <p>hello</p>
+                <div class="h-full bg-obsidian-900 grid grid-cols-3 grid-rows-5">
+                    <div class="col-span-3 row-span-1 bg-obsidian-900  items-center">
+                        <div class="flex items-center justify-center justify-items-center flex-col h-full">
+                            <div class="flex items-start justify-center justify-items-center flex-col">
+                                <h3 class="text-3xl font-bold text-obsidian-100">Sitar</h3>
+                                <h4 class="text-xl font-regular text-obsidian-100">Portfolio</h4>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-span-3 row-span-3 bg-blue-500">
+                    <div class="col-span-3 row-span-3 bg-obsidian-800">
                         <div class="flex items-center justify-center justify-items-center h-full">
-                            <p>hello</p>
+                            <ul>
+                                <li>Home</li>
+                                <li>Experience</li>
+                                <li>About me</li>
+                                <li>Projects</li>
+                                <li>Contact</li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="col-span-3 row-span-1 bg-green-500">
-                        <div class="flex items-center justify-center justify-items-center h-full">
-                            <p>hello</p>
+                    <div class="col-span-3 row-span-1 bg-obsidian-900">
+                        <div class="flex items-center justify-center justify-items-center flex-col h-full">
+                            <div class="flex items-start justify-center justify-items-center flex-col">
+                                <ul>
+                                    <li>Hello</li>
+                                    <li>Language</li>
+                                    <li>Login</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
