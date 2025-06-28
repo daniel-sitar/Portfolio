ba30771e56de92597359f1dc11416738a47ddb04
daniel.sitar <sitarpublic@gmail.com>
2025-06-28 11:57:23 +0200

    add animation + icons

--- portfolio/src/Views/HomeView.vue
+++ portfolio/src/Views/HomeView.vue
@@ -2,38 +2,40 @@
     <div class="home">
         <div class="bg-obsidian-600 h-screen w-screen grid grid-cols-10 grid-rows-10 gap-4">
             <div class="col-span-1 row-span-10 bg-dark-400">
-                <div class="h-full bg-obsidian-900 grid grid-cols-3 grid-rows-5">
-                    <div class="col-span-3 row-span-1 bg-obsidian-900  items-center">
-                        <div class="flex items-center justify-center justify-items-center flex-col h-full">
-                            <div class="flex items-start justify-center justify-items-center flex-col">
-                                <h3 class="text-3xl font-bold text-obsidian-100">Sitar</h3>
-                                <h4 class="text-xl font-regular text-obsidian-100">Portfolio</h4>
+                <nav class="h-full bg-obsidian-900 grid grid-cols-3 grid-rows-5" aria-label="Main sidebar navigation">
+                        <div class="col-span-3 row-span-1 bg-obsidian-900  items-center">
+                            <div class="flex items-center justify-center justify-items-center flex-col h-full">
+                                <div class="flex items-start justify-center justify-items-center flex-col">
+                                    <router-link to="/"><h3 class="text-3xl font-bold text-obsidian-100 hover:text-sky-400 hover:font-black transition-transform duration-200 delay-100 ease-in-out ">Sitar</h3></router-link>
+                                    <h4 class="text-xl font-regular text-obsidian-100">Portfolio</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-span-3 row-span-3 bg-obsidian-800">
-                        <div class="flex items-center justify-center justify-items-center h-full">
-                            <ul>
-                                <li>Home</li>
-                                <li>Experience</li>
-                                <li>About me</li>
-                                <li>Projects</li>
-                                <li>Contact</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-span-3 row-span-1 bg-obsidian-900">
-                        <div class="flex items-center justify-center justify-items-center flex-col h-full">
-                            <div class="flex items-start justify-center justify-items-center flex-col">
-                                <ul>
-                                    <li>Hello</li>
-                                    <li>Language</li>
-                                    <li>Login</li>
-                                </ul>
+                        <div class="col-span-3 row-span-3 bg-obsidian-800">
+                            <div class="flex items-center justify-center justify-items-center h-full">
+                                <div class="flex items-start justify-center justify-items-center flex-col">
+                                    <ul>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Home</a></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Experience</a></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">About me</a></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Projects</a></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Contact</a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                        <div class="col-span-3 row-span-1 bg-obsidian-900">
+                            <div class="flex items-center justify-center justify-items-center flex-col h-full">
+                                <div class="flex items-start justify-center justify-items-center flex-col">
+                                    <ul>
+                                        <li>Light mode</li>
+                                        <li>Language</li>
+                                        <li>Login</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                </nav>
             </div>
             <div class="col-span-9 row-span-10 bg-red-300">
                 <p>hello</p>
