c6b2a3096f7d03f8fdb923967247d2832ee2c324
daniel.sitar <sitarpublic@gmail.com>
2025-07-28 12:35:45 +0200

    change link to router link + add translate button

--- portfolio/src/Views/HomeView.vue
+++ portfolio/src/Views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <div class="bg-obsidian-600 h-screen w-screen grid grid-cols-10 grid-rows-10 gap-4">
+        <div class="bg-obsidian-700 h-screen w-screen grid grid-cols-10 grid-rows-10 gap-4">
             <div class="col-span-1 row-span-10 bg-dark-400">
                 <nav class="h-full bg-obsidian-900 grid grid-cols-3 grid-rows-5" aria-label="Main sidebar">
                         <div class="col-span-3 row-span-1 bg-obsidian-900  items-center">
@@ -13,20 +13,23 @@
                         </div>
                         <div class="col-span-3 row-span-7 bg-obsidian-800">
                             <div class="flex items-start justify-center justify-items-center h-full">
-                                <div class="flex items-start justify-center justify-items-center flex-col my-4">
-                                    <ul class="space-y-4">
-                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Home</a></li>
-                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Experience</a></li>
-                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">About me</a></li>
-                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Projects</a></li>
-                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><a href="#">Contact</a></li>
+                                <div class="flex items-center justify-center justify-items-center flex-col my-4 w-full">
+                                    <ul class="space-y-4 text-start">
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><router-link to="/">Home</router-link></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><router-link to="/">Experience</router-link></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><router-link to="/about">About me</router-link></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><router-link to="/">Projects</router-link></li>
+                                        <li class=" transition-transform hover:underline hover:font-bold hover:scale-110 decoration-sky-400 decoration-2 duration-200 delay-100 ease-in-out"><router-link to="/">Contact</router-link></li>
                                     </ul>
+                                    <div class="flex items-center justify-center justify-items-center my-4 space-y-4">
+                                        <button type="button" class="text-obsidian-100 bg-sky-800 hover:bg-sky-900 hover:text-lg focus:ring-4 focus:ring-sky-800  rounded-lg px-4 py-2.5 me-2 mb-2 text-md transition-transform decoration-2 duration-200 delay-100 ease-in-out">Language</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                 </nav>
             </div>
-            <div class="col-span-9 row-span-10 bg-red-300 text-black">
+            <div class="col-span-9 row-span-10 bg-obsidian-700 text-obsidian-100">
                 <p>hello</p>
             </div>  
         </div>
